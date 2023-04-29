57bc62366b673059b44fdeff2074f5a187a928e0
disantec <disantec@gmail.com>
2023-04-29 16:28:32 -0500

    initial commit of logic for driving motor

---
+++ main.ino
@@ -0,0 +1,14 @@
+void setup() {
+    // declare pins for inputs
+
+
+    // declare pins for outputs
+
+}
+
+void loop() {
+
+    // create logic for setting what motor drive setting to be in
+    // begin running loop for evaluating wheel speed and driving motors
+
+}
